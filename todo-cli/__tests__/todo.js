7d93b7d206eca99a6d33eee74e2f2a0675d52a7f
Y-Dharvik <21951a0536@iare.ac.in>
2023-12-19 14:08:02 +0530

    tests for all functions

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,16 +1,27 @@
 /* eslint-disable no-undef */
 const todolist = require("../todo");
-const { all, markAsComplete, add } = todolist();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todolist();
 
 describe("Todo List test suite", () => {
   beforeAll(() => {
+    const today = new Date().toISOString().split("T")[0];
+    const yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
+      .toISOString()
+      .split("T")[0];
+    const tomorrow = new Date(new Date().setDate(new Date().getDate() + 1))
+      .toISOString()
+      .split("T")[0];
     add({
-      title: "Test todo",
-      dueDate: new Date().toISOString().split("T")[0],
+      title: "Overdue Task 1",
+      dueDate: yesterday,
       completed: false,
     });
+    add({ title: "Overdue Task 2", dueDate: yesterday, completed: true }); // Completed tasks should not be considered overdue
+    add({ title: "Current Task", dueDate: today, completed: false });
+    add({ title: "Future Task", dueDate: tomorrow, completed: false });
   });
-  test("Should add new todo", () => {
+
+  test("A test that checks creating a new todo.", () => {
     const listCount = all.length;
     add({
       title: "Test todo",
@@ -20,9 +31,37 @@ describe("Todo List test suite", () => {
     expect(all.length).toBe(listCount + 1);
   });
 
-  test("Should mark as complete", () => {
+  test("A test that checks marking a todo as completed", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("A test that checks retrieval of overdue items", () => {
+    const overdueItems = overdue();
+
+    // Assert the number of overdue items
+    expect(overdueItems.length).toBe(2);
+
+    // Assert the title of the overdue item
+    expect(overdueItems[0].title).toBe("Overdue Task 1");
+  });
+
+  test("A test that checks retrieval of due today items", () => {
+    const dueTodayItems = dueToday();
+
+    expect(dueTodayItems[0].dueDate).toBe(
+      new Date().toISOString().split("T")[0],
+    );
+    // console.log(dueTodayItems)
+
+    expect(dueTodayItems[0].title).toBe("Current Task");
+  });
+
+  test("A test that checks retrieval of due later items", () => {
+    const dueLaterItems = dueLater();
+
+    expect(dueLaterItems.length).toBe(1);
+
+    expect(dueLaterItems[0].title).toBe("Future Task");
   });
 });
